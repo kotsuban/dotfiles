b35bdbb65c986c9683d7d6e899fcd46e8dd5d310
kotsuban <antontamazlukar@gmail.com>
2025-06-25 08:39:09 +0200

    Fix treesiter-context after update

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -4,6 +4,9 @@ return {
     config = function()
       -- Remove ugly stupid underline in the context thing.
       vim.api.nvim_set_hl(0, "TreesitterContextBottom", { gui = nil })
+
+      -- https://github.com/nvim-treesitter/nvim-treesitter-context/issues/620
+      require("treesitter-context").setup({})
     end,
     opts = function()
       return { mode = "topline" }
