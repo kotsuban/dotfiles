436ff2c029961270c5ca01f5907bf7745eee7f0d
kotsuban <antontamazlukar@gmail.com>
2025-08-21 08:45:27 +0200

    Remove mini plugins

--- nvim/.config/nvim/plugin/mini.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({
-	"https://github.com/echasnovski/mini.icons",
-	"https://github.com/echasnovski/mini.ai",
-})
-
-require("mini.icons").setup()
-require("mini.ai").setup({ n_lines = 500 })
