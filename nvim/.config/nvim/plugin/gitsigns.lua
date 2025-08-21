299c844d403844c1dc4217958763534de0a25d29
kotsuban <antontamazlukar@gmail.com>
2025-08-21 08:59:42 +0200

    Update gitsigns config
    Replace git status symbols with vertical line.
    Hide git blame by default.
    Remove unused keymaps.

--- nvim/.config/nvim/plugin/gitsigns.lua
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -1,16 +1,11 @@
 vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" }, { load = true })
 
 require("gitsigns").setup({
-  current_line_blame = true,
-  current_line_blame_opts = {
-    delay = 500,
-  },
   signs = {
-    add = { text = "+" },
-    change = { text = "~" },
-    delete = { text = "-" },
-    topdelete = { text = "-" },
-    changedelete = { text = "~" },
+    add = { text = "┃" },
+    change = { text = "┃" },
+    delete = { text = "┃" },
+    topdelete = { text = "┃" },
   },
   on_attach = function(bufnr)
     local gitsigns = require("gitsigns")
@@ -21,7 +16,6 @@ require("gitsigns").setup({
       vim.keymap.set(mode, l, r, opts)
     end
 
-    -- Navigation
     map("n", "]c", function()
       if vim.wo.diff then
         vim.cmd.normal({ "]c", bang = true })
@@ -29,7 +23,6 @@ require("gitsigns").setup({
         gitsigns.nav_hunk("next")
       end
     end, { desc = "Jump to next git [c]hange" })
-
     map("n", "[c", function()
       if vim.wo.diff then
         vim.cmd.normal({ "[c", bang = true })
@@ -37,29 +30,13 @@ require("gitsigns").setup({
         gitsigns.nav_hunk("prev")
       end
     end, { desc = "Jump to previous git [c]hange" })
-
-    -- Actions
-    -- visual mode
-    map("v", "<leader>gs", function()
-      gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end, { desc = "git [s]tage hunk" })
-    map("v", "<leader>gr", function()
-      gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end, { desc = "git [r]eset hunk" })
-    -- normal mode
-    map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
-    map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
     map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
-    map("n", "<leader>gu", gitsigns.stage_hunk, { desc = "git [u]ndo stage hunk" })
     map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
     map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
-    map("n", "<leader>gb", gitsigns.blame_line, { desc = "git [b]lame line" })
     map("n", "<leader>gd", gitsigns.diffthis, { desc = "git [d]iff against index" })
+    map("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
     map("n", "<leader>gD", function()
       gitsigns.diffthis("@")
     end, { desc = "git [D]iff against last commit" })
-    -- Toggles
-    map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
-    map("n", "<leader>tD", gitsigns.preview_hunk_inline, { desc = "[T]oggle git show [D]eleted" })
   end,
 })
