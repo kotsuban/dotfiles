090bcfdf1b6815ab664f5149df04fd9c5b7b1a3a
kotsuban <anthonykotsuban@gmail.com>
2025-08-28 02:40:01 +0200

    Move git related plugins to one file

--- nvim/.config/nvim/plugin/git.lua
+++ nvim/.config/nvim/plugin/git.lua
@@ -1,4 +1,7 @@
-vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" }, { load = true })
+vim.pack.add({
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/tpope/vim-fugitive"
+}, { load = true })
 
 require("gitsigns").setup({
   signs = {
