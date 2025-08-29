ae87740d861af972ca628240c80fadbec6adb2c0
kotsuban <anthonykotsuban@gmail.com>
2025-08-29 10:24:03 +0200

    Remove current line highlighting

--- nvim/.config/nvim/plugin/init.lua
+++ nvim/.config/nvim/plugin/init.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.statuscolumn = '%C%s%=%{v:relnum?v:relnum:v:lnum} '
-vim.wo.cursorline = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.winborder = "rounded"
