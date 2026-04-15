2d583cb5552dd15ec3330fd75aff93430a5689ad
kotsuban <anthonykotsuban@gmail.com>
2026-04-16 01:36:19 +0200

    Update dwm configuration
    Remove 1px border on focused window.
    Move topbar to the bottom.
    Remove unused Gimp from rules struct.

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=18";
 static const char col_gray1[]       = "#222222";
@@ -27,7 +27,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Chromium",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
