9c0ab22233c635eddc6da06b445d2dd5a7b93371
kotsuban <anthonykotsuban@gmail.com>
2026-04-17 01:34:18 +0200

    Update default dwm color theme
    Use catpuccin colors by default.

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=18";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#11111b";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#cdd6f4";
+static const char col_gray4[]       = "#11111b";
+static const char col_cyan[]        = "#89b4fa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
