f18ca7513ed389c5cb306daf6701d1fb86abc2dc
kotsuban <anthonykotsuban@gmail.com>
2026-06-25 09:07:36 +0200

    Reduce font-size

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=18";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=14" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=14";
 static const char col_gray1[]       = "#11111b";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#cdd6f4";
